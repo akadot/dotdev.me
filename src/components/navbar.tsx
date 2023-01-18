89542ca6aaa7324426da5bf6b8b566502a27e625
Murilo Henrique de Oliveira <lab3.sp.prnf3regiao@pgfn.gov.br>
2023-01-18 13:37:34 -0300

    :lipstick: Theming

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,20 +3,14 @@ import { ThemeContext } from 'src/context/ThemeContext'
 import Link from 'next/link'
 import style from '../styles/Navbar.module.scss'
 
+interface HandleThemeProps{
+    handleTheme: (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void
+}
 
-export default function Navbar(){
-    const {theme, setTheme} = useContext(ThemeContext);
-
-    function handleTheme(){
-        if(theme === 'dark'){
-            setTheme('light')
-        }else if(theme === 'light'){
-            setTheme('dark')
-        }
-    }
+export default function Navbar(handleTheme:HandleThemeProps){
 
     return (
-        <section className={`${style.container} ${theme}`}>
+        <section className={`${style.container}`}>
             {/* Logo */}
             <h1>Logo</h1>
             {/* Menu */}
@@ -28,7 +22,7 @@ export default function Navbar(){
             </nav>
             {/* Theme */}
             <section>
-                <button onClick={handleTheme}>Change Theme</button>
+                <button onClick={() => handleTheme}>Change Theme</button>
                 <h1>Lang</h1>
             </section>
         </section>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,23 @@
 import type { AppProps } from 'next/app'
+import {useContext} from 'react'
+import { ThemeProvider, ThemeContext } from 'src/context/ThemeContext'
 import Head from 'next/head'
-import { ThemeProvider } from 'src/context/ThemeContext'
 import Menu from '../components/navbar'
 
 import '../styles/globals.scss'
 
 export default function App({ Component, pageProps }: AppProps) {
+  const {theme, setTheme} = useContext(ThemeContext);
+
+  function handleTheme(){
+    console.log(theme)
+    if(theme === 'dark'){
+        setTheme('light')
+    }else if(theme === 'light'){
+        setTheme('dark')
+    }
+  }
+
   return (
     <>
       <Head>
@@ -15,8 +27,8 @@ export default function App({ Component, pageProps }: AppProps) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <ThemeProvider>
-        <section>
-          <Menu/>
+        <section className={`${theme} background`} id="wrapper">
+          <Menu handleTheme={handleTheme}/>
           <Component {...pageProps} />
         </section>
       </ThemeProvider>
