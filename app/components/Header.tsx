d05726388ac3e6f70a34058aae44964898fefa90
akadot <muriloholiveira@hotmail.com>
2023-10-28 10:24:49 -0300

    :fire: Removing lang selector

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,9 +1,9 @@
 "use client"
-import {useState} from 'react'
+import { useState } from 'react'
 import Link from 'next/link'
 
-import {FiSun, FiMoon} from 'react-icons/fi'
-import {LuLanguages} from 'react-icons/lu'
+import { FiSun, FiMoon } from 'react-icons/fi'
+import { LuLanguages } from 'react-icons/lu'
 
 import styles from '@/app/styles/components/header.module.scss'
 
@@ -11,11 +11,11 @@ export default function Header() {
     const [lang, setLang] = useState("en");
     const [theme, setTheme] = useState("dark");
 
-    const changeThemeTo = (theme : string) => {
+    const changeThemeTo = (theme: string) => {
         setTheme(theme);
     }
 
-    const changeLangTo = (lang : string) => {
+    const changeLangTo = (lang: string) => {
         setLang(lang);
     }
 
@@ -29,13 +29,8 @@ export default function Header() {
                 <Link className={styles.pages} href="/projects">projects</Link>
                 <Link className={styles.pages} href="/tetris">tetris</Link>
             </nav>
-            <section className={styles.switches}>
-                <section className={styles.langs}>
-                {lang == "en" ? <span title='to pt-br' onClick={() => changeLangTo("br")}>en</span> : <span title='to en-us' onClick={() => changeLangTo("en")}>br</span>}
-                </section>
-                <section className={styles.theme}>
-                    {theme == "dark" ? <FiMoon size='1rem' title='to light' onClick={() => changeThemeTo("light")}/> : <FiSun size='1rem' title='to dark' onClick={() => changeThemeTo("dark")}/>}
-                </section>
+            <section className={styles.theme}>
+                {theme == "dark" ? <FiMoon size='1.2rem' title='to light' onClick={() => changeThemeTo("light")} /> : <FiSun size='1.2rem' title='to dark' onClick={() => changeThemeTo("dark")} />}
             </section>
         </header>
     )
