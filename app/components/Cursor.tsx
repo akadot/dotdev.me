3a8942a164280875106c04f1e7580b551c366055
Murilo Oliveira <murilo.oliveira@pentagro.com.br>
2023-12-04 08:06:57 -0300

    :lipstrick: Creating a component to cursor

---
+++ app/components/Cursor.tsx
@@ -0,0 +1,29 @@
+"use client"
+import {useEffect, useState} from 'react';
+import { motion } from "framer-motion"
+
+export default function Cursor() {
+    const [mousePos, setMousePos] = useState({x:0,y:0})
+  
+    useEffect(() => {
+      const mouseMove = ( event: MouseEvent )  => {
+        setMousePos({x:event.clientX, y:event.clientY});
+      }
+  
+      window.addEventListener("mousemove", mouseMove);
+  
+      return () => window.removeEventListener("mousemove", mouseMove);
+    }, []);
+  
+    const cursorVariants = {
+      default:{x:mousePos.x, y:mousePos.y}
+    };
+
+    return (
+        <motion.section 
+            className="customCursor"
+            variants={cursorVariants}
+            animate="default"
+          />
+    )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,50 +1,30 @@
-"use client"
-import {useEffect, useState} from 'react';
-import { motion } from "framer-motion"
-
-// import { Metadata } from 'next'
+import { Metadata } from 'next'
 
 import '@/app/styles/globals.scss'
 
 import Header from '@/app/components/Header'
+import Cursor from '@/app/components/Cursor'
 
-// export const metadata: Metadata = {
-//   title: 'dot',
-// }
+
+export const metadata: Metadata = {
+  title: 'dot',
+}
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  const [mousePos, setMousePos] = useState({x:0,y:0})
   
-  useEffect(() => {
-    const mouseMove = ( event: MouseEvent )  => {
-      setMousePos({x:event.clientX, y:event.clientY});
-    }
-
-    window.addEventListener("mousemove", mouseMove);
-
-    return () => window.removeEventListener("mousemove", mouseMove);
-  }, []);
-
-  const cursorVariants = {
-    default:{x:mousePos.x, y:mousePos.y}
-  };
 
   return (
     
     <html lang="pt-br">
       <body>
-          <motion.section 
-            className="customCursor"
-            variants={cursorVariants}
-            animate="default"
-          />
           <Header/>
           {children}
           <span>made with 🌻 ☕ 🎧 💖 by myself ©2023</span>
+          <Cursor/>
         </body>
     </html>
   )
