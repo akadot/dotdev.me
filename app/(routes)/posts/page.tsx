5527a26c674168adf8239367239fab8d5c2e0cf8
Murilo Oliveira <murilo.oliveira@pentagro.com.br>
2023-12-20 10:28:08 -0300

    :boom: Adding filter to posts (not working yet)

--- app/(routes)/posts/page.tsx
+++ app/(routes)/posts/page.tsx
@@ -5,9 +5,11 @@ import { DevPosts } from '@/app/types/posts'
 import {formatDate} from '@/app/utils/formatter'
 
 import styles from '@/app/styles/pages/posts.module.scss'
+import { useState } from 'react';
 
-async function getData(): Promise<DevPosts[]> {
+async function getData(): Promise<{posts:DevPosts[], tags:string[]}> {
     let allPosts: DevPosts[] = [];
+    let allTags: string[] = [];
 
     await fetch("https://dev.to/api/articles/latest?username=akadot_")
         .then(async (res) => {
@@ -22,23 +24,49 @@ async function getData(): Promise<DevPosts[]> {
                     url: post.url,
                     lang: post.description.includes("PT-BR") ? 'br' : 'en'
                 })
+
+                for(let tag of post.tag_list){
+                    if(!allTags.includes(tag)) allTags.push(tag)
+                }
             }
 
         }).catch(() => {
             console.warn("Void Posts.")
         })
 
-    return allPosts;
+    return {posts: allPosts, tags: allTags};
 }
 
+
 export default async function Page() {
-    const posts = await getData();
+    const {posts, tags} = await getData();
+
+    const [filtered, setFiltered] = useState(posts);
+
+    const FilterPosts = (filter : string) : void => {
+        if(filter == "all") setFiltered(posts);
+        
+        if(tags.includes(filter)){
+            let postsFound = posts.filter(item => item.tags.includes(filter));
+
+            if(postsFound.length > 0) setFiltered(postsFound);
+        }
+    }
 
     return (
         <main className={styles.container}>
             <p>📝 Some cool description.</p>
+            <section className={styles.filters}>
+                <span onClick={() => FilterPosts("all")}>All</span>
+                {tags.map((tag, idx) => 
+                    {
+                        return (
+                            <span key={`${idx}-${tag}`} onClick={() => FilterPosts(tag)}>{tag}</span>
+                        )
+                    })}
+            </section>
             <section className={styles.posts_list}>
-                {posts.map(item => {
+                {filtered.map(item => {
                     return (
                         <Link key={item.id} className={styles.post} href={item.url} target='_blank'>
                             <h2>{item.title}</h2>
