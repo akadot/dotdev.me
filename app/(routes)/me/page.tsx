427212bc3d31073c1e7adf84b7dc0fe612b48a7a
akadot <muriloholiveira@hotmail.com>
2023-12-25 22:05:09 -0300

    :memo: Writing personal info

--- app/(routes)/me/page.tsx
+++ app/(routes)/me/page.tsx
@@ -8,9 +8,10 @@ export default function Page() {
   return (
     <main className={styles.container}>
       <section className={styles.about}>
-        <p>Hi, I&apos;m <strong> Murilo Oliveira</strong>.</p>
-        <p>Mussum Ipsum, cacilds vidis litro abertis. Interagi no mé, <strong>cursus</strong> quis, vehicula ac nisi. Praesent vel viverra nisi. Mauris aliquet <strong>nunc</strong>, <strong>nan</strong>, <strong>amis</strong> non turpis scelerisque, eget. Quem num gosta di mim que vai caçá sua turmis! Suco de cevadiss, é um leite divinis, qui tem lupuliz, matis, aguis e fermentis.</p>
-        <p>Mais vale um bebadis conhecidiss, que um <strong>solis</strong> alcoolatra anonimis. Cevadis im ampola pa arma uma pindureta. Atirei o pau no gatis, per gatis num morreus. Per aumento <strong>derila</strong> de cachacis, eu reclamis.</p>
+        <p>Hi, I&apos;m <strong> Murilo Oliveira</strong>, a problem-solver, house gardener and an eternal student.</p>
+        <p>Building rich and solid applications with <strong>C#</strong>, <strong>Javascript</strong>, <strong>Typescript</strong>, <strong>React</strong>, <strong>Next</strong> and <strong>Vue</strong>.</p>
+        <p>Also drawing smart interfaces in <strong>Figma</strong> and writing cool stuffs about technology or anything else on my <strong>DEV.to</strong> and <strong>Medium</strong> account.</p>
+        <p>See you.</p>
         <Socials />
       </section>
       <section className={styles.side_content}>
